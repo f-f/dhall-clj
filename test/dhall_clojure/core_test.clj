f709f977fe4ff9bbd90008278aad794a7370a113
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-03-27 22:25:23 +0300

    Setup tests for parsing, so we can go TDD

---
+++ test/dhall_clojure/core_test.clj
@@ -0,0 +1,37 @@
+(ns dhall-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [dhall-clojure.core :refer [input]]))
+
+(def pairs
+  [;; Primitive Expressions
+   ;;; Double
+   ["2.0"             2.0]
+   ["-2.0"            -2.0]
+   ;; TODO: exponent
+   ;;; Natural
+   ["+1"              1]
+   ["+12"             12]
+   ;;; Integer
+   ["1"               1]
+   ["-12"             -12]
+   ["0"               0]
+   ;;; Text
+   ["\"ABC\""         "ABC"]
+   ["\"\""            ""]
+   ;;; Record Type or Literal
+   ;;; Union Type or Literal
+   ;;; Non-Empty List
+   ["[1]"             '(1)]])
+   ;["[1 2 3]"         '(1 2 3)]])
+   ;["[\"A\"]"         '("A")]])
+   ;;; Import (TODO: all the kinds of imports)
+   ;;; Identifier
+   ;;; Reserved
+   ;;; Expression in parens
+   
+
+(deftest input-parsing
+  (doseq [[dhall clj-form] pairs]
+    (testing (str "Dhall expr: " dhall)
+      (let [parsed (input dhall)]
+        (is (= clj-form parsed))))))
