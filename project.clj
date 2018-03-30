a377d4dd3b08090a8e5af35448937608e637aa7d
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-03-30 19:21:18 +0300

    Copy grammar file to resources every time lein starts

--- project.clj
+++ project.clj
@@ -8,6 +8,8 @@
                  [org.clojure/tools.reader "1.0.3"]
                  [org.clojure/core.match "0.3.0-alpha5"]]
                  ;;[instaparse "1.4.8"]])
+  :plugins [[lein-shell "0.5.0"]]
+  :prep-tasks [["shell" "cp" "dhall-lang/standard/dhall.abnf" "resources/"] "javac" "compile"]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.10.0-alpha2"]]
                    :plugins [[com.jakemccrary/lein-test-refresh "0.22.0"]]}
              :uberjar {:aot :all}}
