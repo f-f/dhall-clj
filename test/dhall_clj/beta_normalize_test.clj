bd774c036094ddc0c0a0b8339cf97db2961ed95a
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-08-29 00:56:08 +0300

    Add (almost all of) the test suite for beta-normalize from the haskell implementation

--- test/dhall_clj/beta_normalize_test.clj
+++ test/dhall_clj/beta_normalize_test.clj
@@ -2,7 +2,10 @@
   (:require  [clojure.test :refer :all]
              [medley.core :refer [map-vals]]
              [dhall-clj.ast :refer :all]
-             [dhall-clj.in.parse :refer [parse expr]]))
+             [dhall-clj.in.parse :refer [parse expr]]
+             [dhall-clj.in.import :refer [resolve-imports]]
+             [dhall-clj.state :as s]
+             [me.raynes.fs :as fs]))
 
 ;; Simple tests from the spec
 
@@ -57,23 +60,77 @@
     (filter (comp not dir?)
             (tree-seq dir? #(.listFiles %) directory))))
 
+(def test-folder "dhall-haskell/tests/normalization")
 
-(defn list-testcases
+(defn all-testcases
   "
   Returns a record of records {'testcase name' {:actual Text, :expected Text}}
   They should be parsed, typechecked and normalized, and checked for equality
   "
   []
-  (let [all-files (list-files "dhall-haskell/tests/normalization")
+  (let [all-files (list-files test-folder)
         map-of-testcases (group-by #(->> % str (drop-last 7) (apply str)) all-files)]
     (map-vals (fn [[actual expected]]
                 {:actual   (slurp actual)
                  :expected (slurp expected)})
               map-of-testcases)))
 
-#_
+(def problematic
+  "Here we list all the tests that blow up, so we categorize and exclude them"
+
+  [;; Exc: no implementation of resolve-imports for nil, called from TextLit
+   "dhall-haskell/tests/normalization/examples/Text/concatMap/1"
+   "dhall-haskell/tests/normalization/examples/Text/concatMap/0"
+   "dhall-haskell/tests/normalization/remoteSystems"
+
+   ;; ast:788, unbound go
+   "dhall-haskell/tests/normalization/examples/Natural/fold/2"
+   "dhall-haskell/tests/normalization/examples/Natural/fold/0"
+   "dhall-haskell/tests/normalization/examples/Natural/fold/1"
+   "dhall-haskell/tests/normalization/examples/List/iterate/0"
+   "dhall-haskell/tests/normalization/examples/List/generate/0"
+   "dhall-haskell/tests/normalization/examples/Natural/enumerate/0"
+   "dhall-haskell/tests/normalization/examples/List/replicate/0"
+
+   ;; No matching clause: :in-raw
+   "dhall-haskell/tests/normalization/examples/List/indexed/0"
+   "dhall-haskell/tests/normalization/examples/List/indexed/1"
+
+   ;; java.lang.StackOverflowError: null
+   "dhall-haskell/tests/normalization/examples/List/shifted/0"
+   "dhall-haskell/tests/normalization/examples/List/shifted/1"
+
+   ;; Actual test failures for results mismatch
+   "dhall-haskell/tests/normalization/examples/List/filter/0"
+   "dhall-haskell/tests/normalization/examples/Text/concatMapSep/0"
+   "dhall-haskell/tests/normalization/examples/Text/concat/0"
+   "dhall-haskell/tests/normalization/examples/List/map/0"
+   "dhall-haskell/tests/normalization/examples/Text/concatSep/0"
+   "dhall-haskell/tests/normalization/examples/List/concat/0"
+   "dhall-haskell/tests/normalization/examples/List/filter/1"
+   "dhall-haskell/tests/normalization/examples/List/unzip/0"
+   "dhall-haskell/tests/normalization/examples/List/fold/2"
+   "dhall-haskell/tests/normalization/examples/Bool/or/0"
+   "dhall-haskell/tests/normalization/examples/List/concatMap/0"
+   "dhall-haskell/tests/normalization/examples/List/any/0"
+   "dhall-haskell/tests/normalization/examples/List/fold/1"
+   "dhall-haskell/tests/normalization/examples/List/all/0"])
+
+(defn valid-testcases []
+  (let [all (all-testcases)]
+    (apply dissoc all problematic)))
+
 (deftest normalization-suite
-  (doseq [[testcase {:keys [actual expected]}] (list-testcases)]
-    (testing testcase
-      (is (= (-> actual   parse expr beta-normalize)
-             (-> expected parse expr beta-normalize))))))
+  (doseq [[testcase {:keys [actual expected]}] (valid-testcases)]
+    (let [parent (fs/parent testcase)
+          f #(fs/with-mutable-cwd
+               (fs/chdir parent)
+               (-> %
+                  parse
+                  expr
+                  (resolve-imports (s/new))
+                  beta-normalize))]
+      ;;(println "TESTCASE" testcase)
+      (testing testcase
+        (is (= (f actual)
+               (f expected)))))))
