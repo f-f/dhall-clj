8d2ddb089ba3d25221036f20fe179db51fb9747d
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-09-12 22:28:29 +0300

    Prepare version for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dhall-clj "0.1.0-SNAPSHOT"
+(defproject dhall-clj "0.1.0"
   :description "Dhall compiler to and from Clojure"
   :url "https://github.com/f-f/dhall-clj"
   :license {:name "Eclipse Public License"
