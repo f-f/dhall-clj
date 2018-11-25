281a3fdbfb65fb00e04684bd97de5393a61f66ca
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2018-11-26 00:31:11 +0100

    Patch the grammar to disambiguate matching of keywords in simple-labels
    Implements https://github.com/dhall-lang/dhall-lang/pull/299
    
    We add a negative lookahead in the simple-label rule, so that
    if a label starts with a keyword we backtrack.
    (Note: there's a space added, to avoid refusing partial matches,
    e.g. if a keyword is "in" we should still accept "index" as a label.

--- test/dhall_clj/parser_test.clj
+++ test/dhall_clj/parser_test.clj
@@ -189,13 +189,13 @@
    ["env:TEST sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855 as Text"
     (map->Import
       {:mode :text
-       :hash? "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"
+       :hash? "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
        :data (->Env "TEST")})]
 
    ["env:\"TE\\\"ST\" sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     (map->Import
       {:mode :code
-       :hash? "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855"
+       :hash? "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
        :data (->Env "TE\\\"ST")})]
 
    ["./relative.dhall"
